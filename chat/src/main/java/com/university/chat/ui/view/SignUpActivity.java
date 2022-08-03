184d4b7168ca81a41179732d9acb6b7fec4e940e
Somto <offorsomto50@gmail.com>
2022-08-03 23:12:29 +0100

    fuctionality in progress

--- chat/src/main/java/com/university/chat/ui/view/SignUpActivity.java
+++ chat/src/main/java/com/university/chat/ui/view/SignUpActivity.java
@@ -1,17 +1,23 @@
 package com.university.chat.ui.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.university.chat.R;
@@ -29,6 +35,8 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         // instantiate views
         textViewLogin = findViewById(R.id.textView_login);
@@ -49,8 +57,7 @@ public class SignUpActivity extends AppCompatActivity {
         clearError(autoCompleteTextViewEmail, textInputLayoutEmail);
         clearError(autoCompleteTextViewPassword, textInputLayoutPassword);
 
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
+
 
         buttonSignUp.setOnClickListener(v -> {
             if (isEmpty(autoCompleteTextViewUsername)){
@@ -63,8 +70,39 @@ public class SignUpActivity extends AppCompatActivity {
                 // validate email
                 if (!isValidEmail(autoCompleteTextViewEmail.getText().toString())){
                     textInputLayoutEmail.setError("Enter valid email");
+                }else if (autoCompleteTextViewPassword.getText().toString().length() < 8){
+                    textInputLayoutPassword.setError("Password must be at least 8 characters");
                 }else {
+                    // assign value to email and password variable
+                    email = autoCompleteTextViewEmail.getText().toString().trim();
+                    password = autoCompleteTextViewPassword.getText().toString().trim();
 
+                    // signing up new users
+                    mAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        //Log.d(TAG, "createUserWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+
+                                        Toast.makeText(SignUpActivity.this, "Authentication successful.",
+                                                Toast.LENGTH_SHORT).show();
+
+                                        clearTextView(autoCompleteTextViewUsername);
+                                        clearTextView(autoCompleteTextViewEmail);
+                                        clearTextView(autoCompleteTextViewPassword);
+                                        //updateUI(user);
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                        Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                        //updateUI(null);
+                                    }
+                                }
+                            });
                 }
             }
         });
@@ -92,5 +130,8 @@ public class SignUpActivity extends AppCompatActivity {
     }
     private boolean isValidEmail(String email) {
         return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+    private void clearTextView(AutoCompleteTextView autoCompleteTextView){
+        autoCompleteTextView.setText(null);
     }
 }
